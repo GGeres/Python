0fcd745b9df5eb11a71ecb7e578bd18afac04b57
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-08-23 18:27:53 -0300

    Update

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -1,1 +1,13 @@
-print("Olá Mundo!")
+print("********************************")
+print("Bem Vindo ao jogo de Advinhações")
+print("********************************")
+
+num_secreto = 42
+
+chute_str = input("Digite seu número: ")
+print("Você chutou ", chute_str)
+chute = int(chute_str)
+if(chute == num_secreto):
+    print("Você acertou!!")
+else:
+    print("Você errou.")
