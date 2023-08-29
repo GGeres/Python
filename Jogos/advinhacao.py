3a3fda861107b07b519903ca7a3f71c4767f1588
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-08-29 15:25:19 -0300

    Update 1

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -4,10 +4,20 @@ print("********************************")
 
 num_secreto = 42
 
+
+
+
 chute_str = input("Digite seu número: ")
 print("Você chutou ", chute_str)
 chute = int(chute_str)
-if(chute == num_secreto):
+
+acertou = chute == num_secreto
+acima = chute > num_secreto
+
+if(acertou):
     print("Você acertou!!")
 else:
-    print("Você errou.")
+    if(acima):
+        print("Seu chute foi maior que o número secreto. Tente novamente.")
+    else:
+        print("Seu chute foi menor que o número secreto. Tente Novamente.")
