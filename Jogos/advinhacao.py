bef6b4ac0844d5077ab1f253cec3cd4829c326ca
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-08-29 16:40:02 -0300

    Update 2

--- Jogos/advinhacao.py
+++ Jogos/advinhacao.py
@@ -1,23 +1,35 @@
+import random
+
 print("********************************")
 print("Bem Vindo ao jogo de Advinhações")
 print("********************************")
 
-num_secreto = 42
+num_secreto = random.randrange(1,101)
+tentativas = 3
 
 
 
+for rodada in range(1, tentativas +1):
+    print("Tentativa {} de {} ". format(rodada,tentativas))
+    chute_str = input("Digite um número de 1 a 100: ")
 
-chute_str = input("Digite seu número: ")
-print("Você chutou ", chute_str)
-chute = int(chute_str)
+    print("Você chutou ", chute_str)
+    chute = int(chute_str)
 
-acertou = chute == num_secreto
-acima = chute > num_secreto
+    if( chute <1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
-if(acertou):
-    print("Você acertou!!")
-else:
-    if(acima):
-        print("Seu chute foi maior que o número secreto. Tente novamente.")
+    acertou = chute == num_secreto
+    acima = chute > num_secreto
+
+    if(acertou):
+        print("Você acertou!!")
+        break
     else:
-        print("Seu chute foi menor que o número secreto. Tente Novamente.")
+        if(acima):
+            print("Seu chute foi maior que o número secreto. Tente novamente.")
+        else:
+            print("Seu chute foi menor que o número secreto. Tente Novamente.")
+
+print("Fim de Jogo!!")
