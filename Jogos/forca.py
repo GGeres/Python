ecea89938ca7bccd85a5faf417c8618d51a03ee2
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-06 16:21:28 -0300

    Update 9

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -36,6 +36,56 @@ def chute_correto(chute, l_acertadas, p_secreta):
             l_acertadas[index] = letra
         index += 1
 
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+    if (erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
 def jogar():
     cabecalho()
 
