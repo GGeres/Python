e9e4bf9674e25741036ac5015e5eefb46b3e448d
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-11 20:11:38 -0300

    Update 11/09

---
+++ OO/conta.py
@@ -0,0 +1,8 @@
+class Conta:
+    def __init__(self, num, tit, saldo, lim):
+        print("Construindo objeto ... {}".format(self))
+        self.num = num
+        self.tit = tit
+        self.saldo = saldo
+        self.lim = lim
+
