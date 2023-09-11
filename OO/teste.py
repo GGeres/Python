b330b47ea2748038e8e2448a52fbd639448c80f1
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-11 13:29:26 -0300

    Update 2

--- OO/teste.py
+++ OO/teste.py
@@ -1,2 +1,12 @@
 def cria_conta(num, tit, saldo, lim):
-    conta: {"num": num, "tit": tit, "saldo": saldo, "lim": lim }
+    conta = {"num": num, "tit": tit, "saldo": saldo, "lim": lim }
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo {}".format(conta["saldo"]))
