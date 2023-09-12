ba3df5516dda952da1a9459594fa776b59c5b36f
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-12 17:34:32 -0300

    Update

--- OO/conta.py
+++ OO/conta.py
@@ -12,8 +12,39 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor) ):
+            self.__saldo -= valor
+        else:
+            print("O valor {} não pode ser sacado.".format(valor))
+
+    def __pode_sacar(self, valor_sacar):
+        valor_disp = self.__lim + self.__saldo
+        return valor_sacar <= valor_disp
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+    def get_tit(self):
+        return self.__tit
+    def get_lim(self):
+        return self.__lim
+    def set_lim(self, lim):
+        self.__lim = lim
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    @property
+    def tit(self):
+        return self.__tit
+    @property
+    def lim(self):
+        return self.__lim
+    @lim.setter
+    def lim(self, lim):
+        self.__lim = lim
+
+
