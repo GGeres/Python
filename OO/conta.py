62d1260f3ef504209950d83b00021493ad184d6e
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-12 16:07:26 -0300

    Update

--- OO/conta.py
+++ OO/conta.py
@@ -1,15 +1,19 @@
 class Conta:
     def __init__(self, num, tit, saldo, lim):
         print("Construindo objeto ... {}".format(self))
-        self.num = num
-        self.tit = tit
-        self.saldo = saldo
-        self.lim = lim
+        self.__num = num
+        self.__tit = tit
+        self.__saldo = saldo
+        self.__lim = lim
 
     def extrato(self):
-        print("O saldo do titular {} é de {}".format(self.tit, self.saldo))
+        print("O saldo do titular {} é de {}".format(self.__tit, self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
