b9be368487d65204462cd405b1c36740b5ca4147
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-13 19:46:24 -0300

    Update

--- Exercícios/Exercicio1.py
+++ Exercícios/Exercicio1.py
@@ -8,26 +8,27 @@ def tabuada(tab):
 def cab():
     print("Bem vindo! Por favor, siga as instruções abaixo:")
     user = str(input("Digite seu nome: "))
-    print(f'Bem vindo {user}.')
+    print(f'Bem vindo {user.title()}.')
 
 def chama_tab():
     tab = int(input("Qual tabuada você quer? "))
     print(tab)
     tabuada(tab)
+    finalizar()
 
 def finalizar():
     print("Deseja usar novamente?")
 
     restart = int(input("(1) Sim - (2) Não"))
 
-    while restart == 1:
+    if restart == 1:
         chama_tab()
+    else:
+        print("Obrigado por utilizar")
 
 def principal():
     cab()
     chama_tab()
-    finalizar()
-
 
 if(__name__ == "__main__"):
     principal()
