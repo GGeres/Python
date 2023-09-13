ab76e132f3fce72924d0939683e7c0617caedf04
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-13 16:23:39 -0300

    Update

--- OO2/filme.py
+++ OO2/filme.py
@@ -2,20 +2,20 @@
 
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
 
 
 class Serie:
     def __init__(self, nome, ano, temp):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temp = temp
 
 
-vingadores = Filme("Os Vingadores", 2012, 160)
-print(vingadores.nome)
+vingadores = Filme("os vingadores", 2012, 160)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} min.')
 
-game_of_thrones = Serie("Game of Thrones", 2011, 8)
+game_of_thrones = Serie("game of thrones", 2011, 8)
 print(f'Nome: {game_of_thrones.nome} - Ano: {game_of_thrones.ano} - Temporadas: {game_of_thrones.temp}')
