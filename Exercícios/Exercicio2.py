6fa01cba8523d1a5095d7af1675e9eaadc981f9a
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-15 15:59:32 -0300

    Update

--- Exercícios/Exercicio2.py
+++ Exercícios/Exercicio2.py
@@ -1,2 +1,12 @@
 def principal():
-    print("*** Bem Vindo ao seu Conversor de Moedas")
+    print("*** Bem Vindo ao seu Conversor de Moedas ***")
+    user = str(input("Digite seu nome: "))
+    print(f'Bem vindo {user.title()}.')
+
+    
+
+
+
+if(__name__ == "__main__"):
+    principal()
+
