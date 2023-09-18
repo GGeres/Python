1fcba6a2429b3b37934e13e60e9e58fadc4d54ac
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-18 16:37:42 -0300

    Update Projeto final

--- Exercícios/Exercicio2.py
+++ Exercícios/Exercicio2.py
@@ -1,30 +1,30 @@
-def pass1():
+def passo1():
     qtd = float(input("Quanto você tem? R$ "))
 
     print(f'Você tem R$ {qtd}.')
-    return qtd
+    coin_select(qtd)
 
 
-def chama_conv(coin, qtd):
+def chama_conv(coin,qtd):
     if coin == 1:
         y = 5.19
         x = qtd
-        print(f'Você pode comprar € {x * y}.')
+        print(f'Você pode comprar € {round(x / y,2 )}.')
     elif coin == 2:
         y = 4.87
         x = qtd
-        print(f'Você pode comprar US$ {x * y}.')
+        print(f'Você pode comprar US$ {round(x / y,2 )}.')
     elif coin == 3:
         y = 6.03
         x = qtd
-        print(f'Você pode comprar £$ {x * y}.')
+        print(f'Você pode comprar £$ {round(x / y,2 )}.')
 
     finalizar()
 
-def coin_select():
+def coin_select(qtd):
     print("Qual moeda você quer comprar?")
     coin = int(input("(1): Euro - (2): Dólar - (3): Libra\n"))
-    return coin
+    chama_conv(coin,qtd)
 
 
 
@@ -35,7 +35,7 @@ def finalizar():
     restart = int(input("(1) Sim - (2) Não\n"))
 
     if restart == 1:
-        pass1()
+        passo1()
     else:
         print("Obrigado por utilizar")
 
@@ -44,9 +44,7 @@ def principal():
     user = str(input("Digite seu nome: "))
     print(f'Bem vindo {user.title()}.')
 
-    pass1()
-    coin_select()
-    chama_conv()
+    passo1()
 
 
 
