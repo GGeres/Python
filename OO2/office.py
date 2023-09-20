e67fc2626c8b33eef1aedfd6d34bad60bb659c87
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-09-20 19:45:58 -0300

    update

--- OO2/office.py
+++ OO2/office.py
@@ -1,4 +1,7 @@
 class Func:
+    def __init__(self, nome):
+        self.nome = nome
+
     def regis_hora(self, horas):
         print('Horas registradas.')
 
@@ -19,16 +22,25 @@ class Alura(Func):
     def busca_perguntas_sem_respostas(self):
         print('Mostrando perguntas não respondidas no fórum')
 
+
+class Hipster:
+    def __str__(self):
+        return f'Hipster, {self.nome}'
+
+
 class Junior(Alura):
     pass
 
-class Pleno(Alura, Caelum):
+class Pleno(Alura, Caelum, Hipster):
     pass
 
-jose = Junior()
+jose = Junior('José')
 jose.busca_perguntas_sem_respostas()
 
-luan = Pleno()
+luan = Pleno('Luan')
 luan.busca_perguntas_sem_respostas()
 luan.bcdmes()
+
 luan.show_task()
+
+print(luan)
