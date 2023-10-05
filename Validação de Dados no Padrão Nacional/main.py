5168cc817696ef0ab4095ed44af5b1b722d4ae84
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-10-05 16:30:26 -0300

    Update

--- Validação de Dados no Padrão Nacional/main.py
+++ Validação de Dados no Padrão Nacional/main.py
@@ -5,7 +5,4 @@ import re
 tele = "5511995217041"
 tele_obj = Tele_Br(tele)
 
-padrao = "([0-9]{2,3})?([0-9]{2})?([0-9]{4,5})([0-9]{4})"
-resposta = re.search(padrao, tele)
-
-print(resposta.group())
+print(tele_obj)
--- Validação de Dados no Padrão Nacional/tele_br.py
+++ Validação de Dados no Padrão Nacional/tele_br.py
@@ -6,10 +6,24 @@ class Tele_Br:
         else:
             raise ValueError("Número Incorreto!!")
 
+    def __str__(self):
+        return self.format_num()
+
     def valida_tel(self,tel):
-        padrao = "[0-9]{2,3}[0-9]{2}[0-9]{4,5}[0-9]{4}"
-        resposta = re.findall(padrao, tel)
+        padrao = "([0-9]{2})?([0-9]{2})?([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao, tel)
         if resposta:
             return True
         else:
             return False
+
+    def format_num(self):
+        padrao = "([0-9]{2})?([0-9]{2})?([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao, self.num)
+        num_format = "+{}({}){}-{}".format(
+            resposta.group(1),
+            resposta.group(2),
+            resposta.group(3),
+            resposta.group(4)
+        )
+        return num_format
