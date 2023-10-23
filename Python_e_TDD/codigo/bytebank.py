45690a399c2762e6481d606770bff832d52d6fc0
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-10-23 17:42:19 -0300

    Update

--- Python_e_TDD/codigo/bytebank.py
+++ Python_e_TDD/codigo/bytebank.py
@@ -15,8 +15,10 @@ class Funcionario:
         return self._salario
 
     def idade(self):
+        split_born_date = self._data_nascimento.split('/')
+        b_year = split_born_date[-1]
         ano_atual = date.today().year
-        return ano_atual - int(self._data_nascimento)
+        return ano_atual - int(b_year)
 
     def calcular_bonus(self):
         valor = self._salario * 0.1
