80c4139479fade190b1097c3152b6b4184f75bcf
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-10-31 17:14:43 -0300

    Update

---
+++ Exercícios/Exercicio3.py
@@ -0,0 +1,31 @@
+#x = 43
+#x = x + 1
+#print(x)
+
+# Exercício 1 acima
+
+#name = input("Digite seu nome: \n")
+#print(name)
+
+# Exercício 2 acima
+
+#horas = ('Digite as horas: \n')
+#hour = input(horas)
+#taxa = ('Digite a taxa: \n')
+#pay = input(taxa)
+#payment = int(hour)*float(pay)
+
+#pFinal = ('O valor a ser pago será: R$ {}'.format(payment))
+
+#print(pFinal)
+
+# Exercício 3 acima
+
+larg = 17
+Alt = 12.0
+
+ver1 = larg/2
+print(ver1)
+type(ver1)
+
+
