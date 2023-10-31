3971c882685af162e6f6f8bb4763036798063954
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-10-31 16:19:10 -0300

    Update

---
+++ Python_e_TDD/codigo/bytebank.py
@@ -0,0 +1,30 @@
+from datetime import date
+
+class Funcionario:
+    def __init__(self, nome, data_nascimento, salario):
+        self._nome = nome
+        self._data_nascimento = data_nascimento
+        self._salario = salario
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def salario(self):
+        return self._salario
+
+    def idade(self):
+        split_born_date = self._data_nascimento.split('/')
+        b_year = split_born_date[-1]
+        ano_atual = date.today().year
+        return ano_atual - int(b_year)
+
+    def calcular_bonus(self):
+        valor = self._salario * 0.1
+        if valor > 1000:
+            valor = 0
+        return valor
+
+    def __str__(self):
+        return f'Funcionario({self._nome}, {self._data_nascimento}, {self._salario})'
