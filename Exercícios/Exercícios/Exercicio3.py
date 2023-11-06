ec59d7e8c055be95bba92f82806877106d50307d
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-11-06 16:58:14 -0300

    Update

--- Exercícios/Exercícios/Exercicio3.py
+++ Exercícios/Exercícios/Exercicio3.py
@@ -112,6 +112,21 @@ print(repetir_Letra())
 '''
 #Exercício 7 acima
 
+'''
+numero = input('Digite um número: ')
+x = int(numero)
+x = x + 1
+print(x)
+'''
+while True:
+    numero = input('Digite um número: ')
+    if numero == 'Pronto':
+        break
+    print(numero)
+print('End')
+
+
+
 
 
 
