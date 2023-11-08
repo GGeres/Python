e9220cfd115a485c62f31f2ca3bd679e96c2b14a
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-11-08 20:12:55 -0300

    Update

---
+++ Exercícios/Exercícios Strings/main.py
@@ -0,0 +1,35 @@
+#Exercício 1
+'''
+aot = 'Eren Yeager'
+#print(len(aot))
+
+indice = len(aot)
+
+
+while indice > 0:
+    letra = aot[indice-1]
+    print(letra)
+    indice = indice - 1
+'''
+
+#Exercício 2
+'''
+exemplo = 'fruta'
+print(exemplo[:])
+'''
+
+#Exercício 3
+'''
+def contagem(palavra,caracter):
+    contagem = 0
+    for letra in palavra:
+        if letra == caracter:
+            contagem = contagem + 1
+    return print(contagem)
+
+palavra = input('Digite uma palavra: \n')
+caracter = input('Qual letra você deseja contar? \n')
+contagem(palavra, caracter)
+'''
+
+#Exercício 4
