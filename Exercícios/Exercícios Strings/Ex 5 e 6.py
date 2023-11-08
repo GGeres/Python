4b4fdcfb5d2e06f9e25a40e805cf05e5b7d9fc1e
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-11-08 20:56:42 -0300

    Update

--- Exercícios/Exercícios Strings/Ex 5 e 6.py
+++ Exercícios/Exercícios Strings/Ex 5 e 6.py
@@ -1,5 +1,5 @@
 #Exercício 5:
-
+'''
 data = '0.8475'
 exemplo = 'X-DSPAM-Confidence:%s' %(data)
 
@@ -7,4 +7,9 @@ last_part = exemplo.find(':')
 #print(last_part)
 
 last_part_text = exemplo[last_part+1:]
-print(last_part_text)
+print(float(last_part_text))
+'''
+aot = 'Guren no Yumiya'
+aot2 = 'Shinzou Wo Sasageyo'
+
+print(aot.replace(aot, aot2))
