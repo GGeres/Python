cb621b8ff4d1cf754a905cd40ec142b5fc63f004
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-11-08 18:20:53 -0300

    Update

--- Exercícios/Exercício Loops/variacao.py
+++ Exercícios/Exercício Loops/variacao.py
@@ -2,6 +2,8 @@
 y = 0
 z = 0
 w = 0
+maximo = None
+minimo = None
 while True:
     try:
         x = input('Digite um número: ')
@@ -11,9 +13,13 @@ while True:
         y = y + num
         w = w + 1
         z = y/w
+        if maximo is None or num > maximo:
+            maximo = num
+        if minimo is None or num < minimo:
+            minimo = num
     except:
         print('Entrada inválida')
 
 
-print(y, w, z)
+print(y, w, z, 'Máximo: ',maximo, 'Mínimo: ', minimo)
 
