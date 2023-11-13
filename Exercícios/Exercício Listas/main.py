660d2a503b8da1a812fdcd74498d78d30daccba6
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-11-13 16:35:51 -0300

    Listas Update

--- Exercícios/Exercício Listas/main.py
+++ Exercícios/Exercício Listas/main.py
@@ -5,3 +5,18 @@ sec = ['Erwin', 'Levi', 'Hange']
 marley = ['Reiner', 'Annie', 'Bertholt', 'Ymir']
 print(aot, sec, marley)
 
+#Mutação de Listas:
+'''
+Em consoles, pode-se substituir os elementos de uma lista em 
+tempo real.
+
+Ex:
+>>> aot = ['1', '2', '3']
+>>> print(aot)
+['1', '2', '3']
+
+>>> aot[1] = '4'
+>>> print(aot)
+['1', '4', '3']
+'''
+
