f9fe6257913e2a0795d864c873f3f0c404f1a2de
Gustavo Jorge Geres <gjgeres@outlook.com>
2023-12-11 16:09:52 -0300

    Update

---
+++ Exercícios/Capítulo 9 - Dicionários/main.py
@@ -0,0 +1,13 @@
+#Dicionários:
+'''
+É como uma lista, mas generalizada
+Numa lista, o índice deve ser um Int, já um dicionário, pode se de (qualquer) tipo.
+
+Um dicionário é como o mapeamento entre um conjunto de índices (as chaves) e
+um conjunto de valores.
+Cada chave localiza um valor
+A associoção entre uma chave e um valor é chamado de ítem.
+
+Exemplo:
+Um dicionário para mapear palavras em Ingles para o Português.
+'''
