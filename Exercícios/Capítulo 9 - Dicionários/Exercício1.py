940852d31faf3ffe8f3e3610967944cd5fbb9eff
Gustavo Jorge Geres <gjgeres@outlook.com>
2023-12-11 17:29:06 -0300

    Update

--- Exercícios/Capítulo 9 - Dicionários/Exercício1.py
+++ Exercícios/Capítulo 9 - Dicionários/Exercício1.py
@@ -8,10 +8,12 @@ except:
     print('File cannot be openned: ', fnome)
     exit()
 
+text = dict()
 for text in fhand:
-    text = dict()
     word = input('Digite uma palavra: ')
     if word in text:
         print(word)
+    elif word == 'exit':
+        exit()
     else:
         print('Word not found')
