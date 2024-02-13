a73407ce893e9dda3c0239ab63c5fb7f7f7b731d
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-02-13 16:46:16 -0300

    Update

--- Python_e_TDD/main.py
+++ Python_e_TDD/main.py
@@ -3,8 +3,3 @@ from codigo.bytebank import Funcionario
 #lucas = Funcionario('Lucas Carvalho', '13/03/2000', 1111)
 #print(lucas.idade())
 
-def teste_idade():
-    func_teste = Funcionario('João', '13/02/2000', 1111)
-    print(f'Teste = {func_teste.idade()}')
-
-teste_idade()
--- Python_e_TDD/tests/test_bytebank.py
+++ Python_e_TDD/tests/test_bytebank.py
@@ -6,10 +6,19 @@ class TestClass:
         # Para testes utilizar o Conceito de Given(Para o Contexto Esperado)/When(Para a Ação)/Then(Para o Desfecho)
         # Given:
         entrada = '13/03/2000'
-        esperado = 23
+        esperado = 24
 
         funcionario_teste = Funcionario('João', entrada, 1111)
         # When:
         resultado = funcionario_teste.idade()
+
+        assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_Gustavo_Geres_deve_retornar_Geres(self):
+        entrada = ' Gustavo Geres ' #Given/Contexto
+        esperado = 'Geres'
+
+        gustavo = Funcionario(entrada, '13/02/2000', 1111)
+        resultado = gustavo.sobrenome() #When
 
         assert resultado == esperado
