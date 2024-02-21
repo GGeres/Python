5b6f59ada7d1a49e941914f577e5e30b6d9e52a2
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-02-21 12:09:18 -0300

    Update

--- Python_e_TDD/tests/test_bytebank.py
+++ Python_e_TDD/tests/test_bytebank.py
@@ -16,13 +16,15 @@ class TestClass:
         assert resultado == esperado
 
 
-'''
-    def test_quando_sobrenome_recebe_Gustavo_Geres_deve_retornar_Geres(self):
-        entrada = ' Gustavo Geres ' #Given/Contexto
-        esperado = 'Geres'
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000
+        entrada_nome = 'Paulo Bragança'
+        esperado = 90000
 
-        gustavo = Funcionario(entrada, '13/02/2000', 1111)
-        resultado = gustavo.sobrenome() #When
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario()
+        resultado = funcionario_teste.salario
 
         assert resultado == esperado
-'''
+
+
