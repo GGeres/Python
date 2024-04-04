c8dfa7b08e8b6151380ad2f288bea5936f738375
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-04-03 22:02:54 -0300

    Update final

--- Python_e_TDD/tests/test_bytebank.py
+++ Python_e_TDD/tests/test_bytebank.py
@@ -1,5 +1,6 @@
-from Python_e_TDD.codigo.bytebank import Funcionario
-
+from codigo.bytebank import Funcionario
+import pytest
+from pytest import mark
 class TestClass:
     def test_quando_idade_recebe_13_02_2000_deve_retornar_24(self):
         # Para um teste sempre começar desse jeito
@@ -26,7 +27,7 @@ class TestClass:
 
         assert resultado == esperado
 
-    #@mark.calcular_bonus
+    @mark.calcular_bonus
     def test_quando_calc_bonus_recebe_1000_retorna_100(self):
         entrada = 1000
         esperado = 100
@@ -35,8 +36,8 @@ class TestClass:
         resultado = funcionario_teste.calcular_bonus()
 
         assert resultado == esperado
-    #@mark.calcular_bonus
-    '''def test_quando_calcular_bonus_recebe_1000000000_retorna_exception(self):
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_1000000000_retorna_exception(self):
         with pytest.raises(Exception):
             entrada = 1000000000
 
@@ -44,4 +45,3 @@ class TestClass:
             resultado = funcionario_teste.calcular_bonus()
 
             assert resultado
-'''
