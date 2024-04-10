c45f4fee215087f115843867974d9b4b7a98a544
Gustavo Jorge Geres <gjgeres@outlook.com>
2024-04-10 11:32:50 -0300

    update

--- Django_2/galeria/models.py
+++ Django_2/galeria/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = 
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
